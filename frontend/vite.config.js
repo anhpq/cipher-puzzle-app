a05659f3ac1ef8620e75036f92db2e320b66971f
anhpq <anhpq60228@gmail.com>
2025-06-02 21:06:48 +0700

    09:06

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5173, // dùng khi chạy dev
+  },
+  preview: {
+    port: parseInt(process.env.PORT) || 4173,
   },
 });
